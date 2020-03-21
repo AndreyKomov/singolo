5cd09b4b56414429d0253ea7ec725681b697145a
ANDREYKOMOV <komov.te@gmail.com>
2020-03-21 13:57:29 +0300

    feat: index, style, script

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const MENU=document.getElementById ("menu");
-const header = document.getElementById("head_menu")
+const MENU = document.getElementById ("menu");
+const HEADER = document.getElementById ("head_menu");
+const BTNLEFT = document.getElementById ("btn_left");
+const BTNRIGHT = document.getElementById ("btn_right");
+const BUTTON = document.getElementById ("btn");
+const CLOSE_BTN = document.getElementById ("close_btn");
 
-header.classList.add("head_menu_sticky");
+HEADER.classList.add("head_menu_sticky");
 
 MENU.addEventListener("click", (event) => {
-  MENU.querySelectorAll("a").forEach(el => el.classList.remove("active"));
+  MENU.querySelectorAll("a").forEach (el => el.classList.remove("active"));
   event.target.classList.add("active");
+});
+
+BUTTON.addEventListener("click", () => {
+  const subject = document.getElementById ("subject1").value;
+  document.getElementById ("result").innerText = subject1;
+  document.getElementById ("message_block").classList.remove ("hidden");
+});
+
+CLOSE_BTN.addEventListener("click", () => {
+  document.getElementById ("result").innerText = "";
+  document.getElementById ("message_block").classList.add ("hidden");
 });
