e2c48b76d9e761e09055dc9ab5d0d35fd95c2324
ANDREYKOMOV <komov.te@gmail.com>
2020-03-30 01:16:03 +0300

    feat: screen on/off

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const MENU = document.getElementById ("menu");
 const HEADER = document.getElementById ("head_menu");
 const BTNLEFT = document.getElementById ("btn_left");
 const BTNRIGHT = document.getElementById ("btn_right");
+const SCREEN = document.getElementById ("screen");
 const BUTTON = document.getElementById ("btn");
 const CLOSE_BTN = document.getElementById ("close_btn");
 
@@ -10,6 +11,11 @@ HEADER.classList.add("head_menu_sticky");
 MENU.addEventListener("click", (event) => {
   MENU.querySelectorAll("a").forEach (el => el.classList.remove("active"));
   event.target.classList.add("active");
+});
+
+SCREEN.addEventListener("click", (event) => {
+  SCREEN.querySelectorAll ("div").forEach (el => el.classList.remove("screen_off"));
+  event.target.classList.add("screen_off");
 });
 
 BUTTON.addEventListener("click", () => {
